a0a60318005b4804b21cfabed6f6d005ee878609
srahal84 <78704150+srahal84@users.noreply.github.com>
2021-02-14 20:54:55 +0100

    Update PullPayment.sol

--- PullPayment.sol
+++ PullPayment.sol
@@ -1,21 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.11;
-
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/math/SafeMath.sol";
 contract PullPayment {
-    mapping(address => uint) credits;
-
-    function allowForPull(address receiver, uint amount) private {
-        credits[receiver] += amount;
-    }
-
-    function withdrawCredits() public {
-        uint amount = credits[msg.sender];
-
-        require(amount != 0);
-        require(address(this).balance >= amount);
-
-        credits[msg.sender] = 0;
-
-        msg.sender.transfer(amount);
-    }
+  using SafeMath for uint256;
+  mapping (address => uint256) public credits;
+  uint256 public total;
+  
+  function asyncSend(address dest, uint256 amount) internal {
+      credits[dest] = credits[dest].add(amount);
+      total = total.add(amount);
+  }
+  
+  function withdrawPayments() public {
+      require(credits[msg.sender] != 0);
+      total = total.sub(credits[msg.sender]);
+      credits[msg.sender] = 0;
+      msg.sender.transfer(credits[msg.sender]);
+  }
 }
